214caa098e0256598c8dc8095a7c973b6c957c71
sudhabhashyam <lakshmisudha9090@gmail.com>
2023-11-16 01:35:15 +0530

    https://github.com/mdalmamunit427/Advanced-React-Portfolio-Projects-Coursera-.git

---
+++ practise.js
@@ -0,0 +1,57 @@
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+
+const tax = 1.20;
+
+function getPrices(taxBoolean) {
+    for (var dish of dishData) {
+        var finalPrice;
+        if (taxBoolean === true) {
+            finalPrice = dish.price * tax
+        }
+        else if (taxBoolean === false) {
+            finalPrice = dish.price
+        }
+        else {
+            console.log("the value should be a boolean")
+            return;
+        }
+    }
+  console.log(`Dish: ${dish.name} Price: $${finalPrice}`);
+   }
+    
+    
+// Implement getDiscount()
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean)
+    if (typeof(guests) === 'number' && guests > 0 && guests < 30) {
+       var discount = 0
+        if (guests < 5) {
+            discount =5
+        }
+        else if (guests >= 5) {
+            discount = 10
+        }
+    console.log('Discount is: $${discount}')
+    }
+    else {
+        console.log("guests should be a number between 0 and 30")
+    }
+}
+    
